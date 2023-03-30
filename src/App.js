5168b3b8fb45b7ba3c0ba201407e1eb1fa5f657a
Vinay <vinaygithub700@gmail.com>
2023-03-30 11:13:43 +0530

    done with olans

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+
 function App() {
   return (
     <div className="App">
-          <Hero/>
-          <Programs/>
-          <Reasons/>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans/>
     </div>
   );
 }
